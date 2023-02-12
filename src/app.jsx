b56be9c2d1bff888c11fdff09df4c6721206cf75
Vivy <Tomitaviorel@yahoo.com>
2023-02-12 09:42:43 +0200

    feat: added general css and font

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,29 @@
+import About from './sections/about/about';
+import Contact from './sections/contact/contact';
+import Faq from './sections/faq/faq';
+import Footer from './sections/footer/footer';
+import Header from './sections/header/header';
+import Nav from './sections/nav/nav';
+import Navbar from './sections/navbar/navbar';
+import Portfolio from './sections/portfolio/portfolio';
+import Services from './sections/services/services';
+import Testimonials from './sections/testimonials/testimonials';
+
 const App = () => {
-  return <div>asdadfaasasdsad</div>;
+  return (
+    <main>
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Faq />
+      <Contact />
+      <Footer />
+      <Nav />
+    </main>
+  );
 };
 
 export default App;
