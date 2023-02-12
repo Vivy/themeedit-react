0b04738fd5100c6e9d0fd5fce75eac70a31e381d
Vivy <Tomitaviorel@yahoo.com>
2023-02-12 13:06:38 +0200

    feat: added header section

---
+++ src/sections/header/data.js
@@ -0,0 +1,10 @@
+import { BsInstagram, BsFacebook, BsTwitter, BsGithub } from 'react-icons/bs'
+
+const data = [
+  { id: 1, link: 'https://instagram.com', icon: <BsInstagram /> },
+  { id: 2, link: 'https://facebook.com', icon: <BsFacebook /> },
+  { id: 3, link: 'https://twitter.com', icon: <BsTwitter /> },
+  { id: 4, link: 'https://github.com', icon: <BsGithub /> },
+]
+
+export default data
--- src/sections/header/header.jsx
+++ src/sections/header/header.jsx
@@ -1,8 +1,38 @@
 import './header.css';
+import header from '../../assets/Ai1.jpg';
+import data from './data';
+
 const Header = () => {
   return (
-    <header>
-      <h2>HEADER</h2>
+    <header id='header'>
+      <div className='container header__container'>
+        <div className='header__profile'>
+          <img src={header} alt='header image' />
+        </div>
+        <h3>Vivy</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. A cumque
+          impedit sunt illum officia, quam corrupti labore facilis? Et atque
+          sunt recusandae dolorum? Quia consequuntur harum pariatur officiis
+          quas, architecto voluptas recusandae beatae asperiores facilis sequi
+          inventore.
+        </p>
+        <div className='header__cta'>
+          <a href='#contact' className='btn primary'>
+            Let`s talk
+          </a>
+          <a href='#portfolio' className='btn light'>
+            My Work
+          </a>
+        </div>
+        <div className='header__socials'>
+          {data.map(({ id, link, icon }) => (
+            <a href={link} key={id} target='_blank' rel='noopener noreferrer'>
+              {icon}
+            </a>
+          ))}
+        </div>
+      </div>
     </header>
   );
 };
