feb732864ae954049b81020c71d7b4a5290f4356
Vivy <Tomitaviorel@yahoo.com>
2023-02-12 12:06:27 +0200

    feat:added media and react icons, did navbar component

--- src/sections/navbar/navbar.jsx
+++ src/sections/navbar/navbar.jsx
@@ -1,8 +1,26 @@
+import logo from '../../assets/mobile.jpg';
 import './navbar.css';
+import data from './data';
+import { IoIosColorPalette } from 'react-icons/io';
+
 const Navbar = () => {
   return (
     <nav>
-      <h2>NAVBAR</h2>
+      <div className='container nav__container'>
+        <a href='index.html' className='nav__logo'>
+          <img src={logo} alt='the logo' />
+        </a>
+        <ul className='nav__menu'>
+          {data.map(({ id, link, title }) => (
+            <li key={id} className='nav__menu'>
+              <a href={link}>{title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id='theme__icon'>
+          <IoIosColorPalette />
+        </button>
+      </div>
     </nav>
   );
 };
