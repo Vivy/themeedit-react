edd7cb3855eb1269949dc23f4628cae82dfddb69
Vivy <Tomitaviorel@yahoo.com>
2023-02-13 09:19:50 +0200

    feat: services component

---
+++ src/sections/services/data.js
@@ -0,0 +1,14 @@
+import { SiAdobexd } from 'react-icons/si'
+import { RiReactjsLine } from 'react-icons/ri'
+import { FaServer } from 'react-icons/fa'
+import { AiFillAppstore } from 'react-icons/ai'
+
+
+const data = [
+  { id: 1, icon: <SiAdobexd />, title: 'UI/UX Design', desc: '  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  ' },
+  { id: 2, icon: <RiReactjsLine />, title: 'Frontend Development', desc: '  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  ' },
+  { id: 3, icon: <FaServer />, title: 'Backend Development', desc: '  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  ' },
+  { id: 4, icon: <AiFillAppstore />, title: 'Appp Development', desc: '  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  lorem  ' },
+]
+
+export default data
--- src/sections/services/services.jsx
+++ src/sections/services/services.jsx
@@ -1,8 +1,23 @@
 import './services.css';
+import data from './data';
+import Card from '../../component/card/card';
+
 const Services = () => {
   return (
     <section id='services'>
-      <h2>SERVICES</h2>
+      <h2>My SERVICES</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      <div className='container services__container'>
+        {data.map(({ id, title, desc, icon }) => (
+          <Card key={id} className='service light'>
+            <div className='service__icon'>{icon}</div>
+            <div className='service__detailes'>
+              <h4>{title}</h4>
+              <p>{desc}</p>
+            </div>
+          </Card>
+        ))}
+      </div>
     </section>
   );
 };
